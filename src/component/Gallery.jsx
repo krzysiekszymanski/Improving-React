521fbef4ecb51ce8fc66cdf3fbfbf738fd26811a
Krzysiek Szymanski <krzysiekszymanski@poczta.onet.pl>
2017-11-01 22:27:38 +0100

    add a event in gallery

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -34,35 +34,70 @@ const Panel = styled.div`
       justify-content: center;
       align-items: center;
     }
+    :nth-child(1) { background-image:url(https://source.unsplash.com/gYl-UtwNg_I/1500x1500); }
+    :nth-child(2) { background-image:url(https://source.unsplash.com/rFKUFzjPYiQ/1500x1500); }
+    :nth-child(3) { background-image:url(https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d); }
+    :nth-child(4) { background-image:url(https://source.unsplash.com/ITjiVXcwVng/1500x1500); }
+    :nth-child(5) { background-image:url(https://source.unsplash.com/3MNzGlQM7qs/1500x1500); }
+    > *:first-child { transform: translateY(-100%); }
+ 
+    > *:last-child { transform: translateY(100%); }
+
+    
     p {
       text-transform: uppercase;
       font-family: 'Amatic SC', cursive;
       text-shadow:0 0 4px rgba(0, 0, 0, 0.72), 0 0 14px rgba(0, 0, 0, 0.45);
       font-size: 2em;
     }
+    
     p:nth-child(2) {
-      font-size: 3em;
+      font-size: 2em;
     }
-    :nth-child(1) { background-image:url(https://source.unsplash.com/gYl-UtwNg_I/1500x1500); }
-    :nth-child(2) { background-image:url(https://source.unsplash.com/rFKUFzjPYiQ/1500x1500); }
-    :nth-child(3) { background-image:url(https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d); }
-    :nth-child(4) { background-image:url(https://source.unsplash.com/ITjiVXcwVng/1500x1500); }
-    :nth-child(5) { background-image:url(https://source.unsplash.com/3MNzGlQM7qs/1500x1500); }
+    
     
  `;
 
 
 
 class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            style: '',
+        }
+    }
+
+    handleClick = (e) => {
+        console.log('działą')
+        if (this.state.style == '') {
+            this.setState({
+                style: '5'
+            })
+        } else {
+            this.setState({
+                style: ''
+            })
+        }
+
+    }
+
+
     render() {
+        let divStyle = {
+            flex: this.state.style
+        }
+        console.log(this.state.style)
         return(
+
         <Panels>
-            <Panel>
+            <Panel onClick={this.handleClick} style={divStyle}>
                 <p>Hey</p>
                 <p>Let's</p>
                 <p>Dance</p>
             </Panel>
-            <Panel>
+            <Panel onClick={this.handleClick} style={divStyle}>
                 <p>Give</p>
                 <p>Take</p>
                 <p>Receive</p>
